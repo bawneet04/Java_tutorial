c42eecdf9e076998e0f562b18a83ba30bbcbad0f
bawne <bawne@10.12.95.234>
2022-01-25 08:46:45 -0500

    Initial commit

--- src/git/Git.java
+++ src/git/Git.java
@@ -15,6 +15,7 @@ public class Git {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Welcome to Github");
     }
     
 }
